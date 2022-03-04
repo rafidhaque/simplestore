6d86f268d13963d18a5a2398824bacd5189cf3fe
rafidhaque <rafhaq4@gmail.com>
2022-03-04 10:31:48 +0600

    new fn

--- src/simpleStore.js
+++ src/simpleStore.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
+import { ethers } from "ethers";
+import SimpleStore_abi from "./SimpleStore_abi.json";
 
 const SimpleStore = () => {
-    const [errorMessage, setErrorMessage] = useState(null);
+    const contractAddress = "0x411CE1cB07F0c36837A7C514dAFA94Fe86597Df5";
+    // change the abi and the contract address (not user adress) each time the contract gets deployed
+
+    const [errorMessage, setErrorMessage] = useState("WHY");
     const [connButtonText, setConnButtonText] = useState("Connect Wallet");
     const [defaultAccount, setDefaultAccount] = useState(null);
+
+    const [provider, setProvider] = useState(null);
+    const [signer, setSigner] = useState(null);
+    const [contract, setContract] = useState(null);
 
     const connectWalletHandler = () => {
         if (window.ethereum) {
@@ -12,7 +21,9 @@ const SimpleStore = () => {
                 .request({ method: "eth_requestAccounts" }) //method on ethereum
                 .then((result) => {
                     // result will contain an array of ethereum accounts
+                    console.log(result);
                     accountChangedHandler(result[0]);
+                    setConnButtonText("Wallet Connected");
                 });
         } else {
             setErrorMessage("Need to install Metamask!");
@@ -21,7 +32,30 @@ const SimpleStore = () => {
 
     const accountChangedHandler = (newAccount) => {
         setDefaultAccount(newAccount);
+        updateEthers();
     };
+
+    function updateEthers() {
+        let tempProvider = new ethers.providers.Web3Provider(window.ethereum); //reads
+        setProvider(tempProvider);
+
+        let tempSigner = tempProvider.getSigner(); //writes
+        setSigner(tempSigner);
+
+        let tempContract = new ethers.Contract(
+            contractAddress,
+            SimpleStore_abi,
+            tempSigner
+        );
+        setContract(tempContract);
+    }
+
+    function getCurrentVal() {
+        console.log(contract);
+        let val = contract.lol().then((val) => {
+            setErrorMessage(val);
+        });
+    }
 
     return (
         <div>
@@ -29,7 +63,9 @@ const SimpleStore = () => {
             <button onClick={connectWalletHandler}> {connButtonText} </button>
             <h3>Address: {defaultAccount}</h3>
 
-            {errorMessage}
+            <button onClick={getCurrentVal}>Get Current Value</button>
+
+            <h3> {errorMessage}</h3>
         </div>
     );
 };
